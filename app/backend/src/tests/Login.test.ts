a8319d99babfb81eab2c573c4d0b36de485103e8
Wilson Maximo <wilson-max@hotmail.com>
2023-11-14 21:21:45 -0300

    Saving progress in tests

--- app/backend/src/tests/Login.test.ts
+++ app/backend/src/tests/Login.test.ts
@@ -8,6 +8,7 @@ import chaiHttp = require('chai-http');
 import { App } from '../app';
 import SequelizeUser from '../database/models/SequelizeUser';
 import { userMock, validUser, invalidEmail, invalidPassword } from './Mocks/UserMock';
+import validToken from './Mocks/validToken';
 
 chai.use(chaiHttp);
 
@@ -28,6 +29,25 @@ describe('User Test', function() {
     expect(status).to.be.equal(200);
     expect(body).to.haveOwnProperty('token');
   });
+
+  it('Verifica se retorna o role do usuário', async () => {
+    sinon.stub(SequelizeUser, 'findOne').resolves(userMock as any);
+
+    const { status, body } =  await chai.request(app).get('/login/role').set('Authorization', `Bearer ${validToken}`);
+    
+    expect(status).to.be.equal(200);
+    expect(body).to.be.equal('admin');
+    
+  })
+
+  it('Verifica que não é possivel acessar a role com Token invalido', async () => {
+    sinon.stub(SequelizeUser, 'findOne').resolves(userMock as any);
+
+    const { status, body } =  await chai.request(app).get('/login/role').set('Authorization', 'Bearer invalidToken');
+    
+    expect(status).to.be.equal(401);
+    expect(body.message).to.be.equal('Expired or invalid token');
+  })
 
   it('Verifica se é possivel fazer login com senha incorreta', async () => {
     sinon.stub(SequelizeUser, 'findOne').resolves(userMock as any);
